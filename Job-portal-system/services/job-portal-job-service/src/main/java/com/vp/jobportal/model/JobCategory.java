b966cdf767e043ecee7884a532ba840bc46c5259
Veeresh5027 <veereshpattar5027@gmail.com>
2026-06-05 12:09:16 +0530

    JobCategory entity created

---
+++ Job-portal-system/services/job-portal-job-service/src/main/java/com/vp/jobportal/model/JobCategory.java
@@ -0,0 +1,53 @@
+package com.vp.jobportal.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "job_categories")
+public class JobCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(unique = true)
+    private String slug;
+
+    private String description;
+
+    private String iconUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private JobCategory parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<JobCategory> subCategories = new ArrayList<>();
+
+    //timestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false, updatable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
