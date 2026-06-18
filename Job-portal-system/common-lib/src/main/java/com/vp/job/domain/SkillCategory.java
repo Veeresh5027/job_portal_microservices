f28d604eaee9873fce3342e8318376b1917676d7
Veeresh5027 <veereshpattar5027@gmail.com>
2026-06-18 21:27:31 +0530

    job skills db design entity added

---
+++ Job-portal-system/common-lib/src/main/java/com/vp/job/domain/SkillCategory.java
@@ -0,0 +1,17 @@
+package com.vp.job.domain;
+
+public enum SkillCategory {
+
+    PROGRAMMING_LANGUAGE,
+    FRAMEWORK,
+    DATABASE,
+    TOOLS,
+    CLOUD_PLATFORM,
+    DEVOPS,
+    DESIGN,
+    SOFT_SKILLS,
+    LANGUAGE,
+    OTHER
+
+
+}
